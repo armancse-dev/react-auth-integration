15c34a971683564d1ee5f5a4efca3acf129e7c69
armancse-dev <armanhossaincse7@gmail.com>
2022-10-17 22:06:25 +0600

    Add Google Sign using custom hook with on State Change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebse from '../../hooks/useFirebase';
 const Login = () => {
+   const {signInUsingGoogle} = useFirebse();
    return (
       <div>
          <h2>Please Login</h2>
-         <button>Google Sign In</button><br/>
+         <button onClick={signInUsingGoogle} >Google Sign In</button><br/>
          <Link to="/register">New User?</Link>
       </div>
    );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import initAuthentication from "../Firebase/Firebase.init";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 
 initAuthentication();
 
@@ -8,7 +8,7 @@ const useFirebse = () =>{
    const [user, setUser] =  useState({});
    const [error, setError] = useState('');
    
-   const auth =  getAuth;
+   const auth =  getAuth();
    const googleProvider = new GoogleAuthProvider();
 
    const signInUsingGoogle = () =>{
@@ -22,6 +22,16 @@ const useFirebse = () =>{
          setError(error.message);
       })
    }
+
+   useEffect( () => {
+      onAuthStateChanged(auth, user => {
+         if(user){
+            console.log('inside state change',user);
+            setUser(user);
+         }
+      })
+   }, [])
+
    return{
       user,
       error,
